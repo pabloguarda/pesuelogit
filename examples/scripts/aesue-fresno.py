01f902d78b67eae4b9292cdc1b6b31729d5be591
pabloguarda <pabloguarda@cmu.edu>
2022-05-24 11:07:51 -0400

    Update aesue-fresno.py

--- examples/scripts/aesue-fresno.py
+++ examples/scripts/aesue-fresno.py
@@ -1,10 +1,4 @@
 '''
-
-Install isuelogit using pip install -q git+https://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@github.com/pabloguarda/isuelogit.git
-
-- Notes:
-        - Think on how to incorporate traffic count data, e.g. an additional term in the loss function
-
 '''
 
 import os
@@ -15,14 +9,18 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 import pandas as pd
+import random
+# import cudf as pd
 import tensorflow as tf
 import isuelogit as isl
 import glob
 
-from src.aesuelogit.aesue import UtilityFunction, AESUELOGIT, Equilibrator, plot_predictive_performance
-from src.aesuelogit.networks import load_k_shortest_paths, read_paths,build_fresno_network
-from src.aesuelogit.etl import get_design_tensor, get_y_tensor, simulate_suelogit_data
+from src.aesuelogit.models import UtilityFunction, AESUELOGIT, NGD
+from src.aesuelogit.visualizations import plot_predictive_performance
+from src.aesuelogit.networks import load_k_shortest_paths, read_paths, build_fresno_network, \
+    Equilibrator, sparsify_OD, ColumnGenerator, read_OD
 
+from src.aesuelogit.etl import get_design_tensor, get_y_tensor, data_curation
 # Path management
 main_dir = str(Path(os.path.abspath('')).parents[1])
 os.chdir(main_dir)
@@ -30,18 +28,51 @@ print('main dir:', main_dir)
 
 isl.config.dirs['read_network_data'] = "input/network-data/fresno/"
 
+# Seed for reproducibility
+_SEED = 2022
+
+np.random.seed(_SEED)
+random.seed(_SEED)
+tf.random.set_seed(_SEED)
+
 # To write estimation report and set seed for all algorithms involving some randomness
 # estimation_reporter = isl.writer.Reporter(
 #     folderpath=isl.config.dirs['output_folder'] + 'estimations/' + 'Fresno', seed=_SEED)
 
+## Build Fresno network
+
 fresno_network = build_fresno_network()
 
-# Paths
-# load_k_shortest_paths(network=fresno_network, k=2, update_incidence_matrices=True)
-# read_paths(network=fresno_network, update_incidence_matrices=True, filename = 'paths-full-model-fresno.csv')
-read_paths(network=fresno_network, update_incidence_matrices=True, filename='paths-fresno.csv')
+## Read OD matrix
+# TODO: specify path to read OD matrix
+read_OD(network=fresno_network, sparse=True)
 
-# Read spatiotemporal data
+# Q = fresno_network.load_OD(sparsify_OD(fresno_network.Q, prop_od_pairs=0.99))
+# #
+# load_k_shortest_paths(network=fresno_network, k=2, update_incidence_matrices=True)
+
+## Read paths
+
+# read_paths(network=fresno_network, update_incidence_matrices=True, filename='paths-fresno.csv')
+read_paths(network=fresno_network, update_incidence_matrices=True, filename = 'paths-full-model-fresno.csv')
+
+## Utility function
+
+features_Z = ['speed_sd', 'median_inc', 'incidents', 'bus_stops', 'intersections']
+# features_Z = ['speed_sd']
+# features_Z = []
+
+utility_function = UtilityFunction(features_Y=['tt'],
+                                   features_Z=features_Z,
+                                   signs={'tt': '-', 'speed_sd': '-', 'median_inc': '+', 'incidents': '-',
+                                          'bus_stops': '-', 'intersections': '-'},
+                                   # initial_values={'tt': 0, 'median_inc': 0, 'incidents': 0}
+                                   )
+
+utility_function.constant_initializer(0)
+
+
+## Read spatiotemporal data
 folderpath = isl.config.dirs['read_network_data'] + 'links/spatiotemporal-data/'
 df = pd.concat([pd.read_csv(file) for file in glob.glob(folderpath + "*fresno-link-data*")], axis=0)
 
@@ -62,55 +93,93 @@ df['period'] = df.period.map(hash)
 #
 # df = pd.concat([df1, df2], axis=0)
 
-features_Z = ['speed_sd', 'median_inc', 'incidents', 'bus_stops', 'intersections']
+## Data processing
 
-# Data cleaning
-# TODO: Perform data imputation of links of time LWLK with travel times equal to 0
-df.loc[df['counts'] <= 0, "counts"] = np.nan
+# df['tt_ff'] = np.tile([link.bpr.tf for link in fresno_network.links],len(df.date.unique())*len(df.hour.unique()))
+# df['tt_ff'] = df['tf_inrix']
 
-# Normalization of features to range [0,1] (TODO: enable this option in get_design_tensor method or see if tensorflow have it)
-df[features_Z + ['tt_avg']] = preprocessing.MinMaxScaler().fit_transform(df[features_Z + ['tt_avg']])
 
-utility_function = UtilityFunction(features_Y=['tt'],
-                                   features_Z=features_Z,
-                                   signs={'tt': '-', 'speed_sd': '-', 'incidents': '-', 'intersections': '-',
-                                          'bus_stops': '-', 'median_inc': '+'},
-                                   # initial_values={'tt': 0, 'median_inc': 0, 'incidents': 0}
-                                   )
+df['tt_avg'] = np.divide(df['length'],df['speed_hist_avg'],
+                         out=np.zeros_like(df['length']), where=df['speed_hist_avg']!=0)
 
-utility_function.constant_initializer(0)
+df['tt_ff'] = np.divide(df['length'],df['speed_ref_avg'],
+                         out=np.zeros_like(df['length']), where=df['speed_ref_avg']!=0)
 
-# TODO: Make sure the dataframe is properly sorted before doing the reshaping into a tensor of multiple dimensions
+
+
+# df.loc[(df['tt_ff'] == 0 ) & (df['link_type'] == 'LWRLK'),'tt_ff']
+
+# df.link_type.value_counts()
+
+# Select only dates used for previous paper
+# df = df.query('date == "2019-10-01"  | date == "2020-10-06"')
+# df = df.query('date == "2019-10-01"')
+# df = df.query('hour == 16')
+
+df = data_curation(df)
+
+# Normalization of features to range [0,1]
+# (TODO: enable normalization in get_design_tensor method. See if tensorflow have it)
+df[features_Z + ['tt_avg'] + ['tt_ff']] \
+    = preprocessing.MinMaxScaler().fit_transform(df[features_Z + ['tt_avg'] + ['tt_ff']])
+
+# df['tt_ff'] = preprocessing.MinMaxScaler().fit_transform(df[['tt_ff']])
 
 n_links = len(fresno_network.links)
+df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
 df['year'] = df.date.dt.year
 X, Y = {}, {}
 
+print(df.query('year == 2019')[['counts', 'tt_ff', 'tt_avg', 'tf_inrix']].describe())
+
+print(df.query('year == 2020')[['counts', 'tt_ff', 'tt_avg', 'tf_inrix']].describe())
+
+#Testing
+# df['tt_avg'] = df['tt_ff']
+
 for year in sorted(df['year'].unique()):
     df_year = df[df['year'] == year]
+
     n_days, n_hours = len(df_year.date.unique()), len(df_year.hour.unique())
 
-    traveltime_data = get_design_tensor(y=df_year['tt_avg'], n_links=n_links, n_days=n_days, n_hours=n_hours)
+    # TODO: Add an assert to check the dataframe is properly sorted before reshaping it into a tensor
+
+    traveltime_data = get_y_tensor(y=df_year[['tt_avg']], n_links=n_links, n_days=n_days, n_hours=n_hours)
     flow_data = get_y_tensor(y=df_year[['counts']], n_links=n_links, n_days=n_days, n_hours=n_hours)
 
-    Y[year] = tf.squeeze(tf.stack([traveltime_data, flow_data], axis=3))
-    X[year] = get_design_tensor(Z=df_year[['tf_inrix'] + features_Z], y=df_year['tt_avg'],
-                                n_links=n_links, n_days=n_days, n_hours=n_hours)
+    Y[year] = tf.concat([traveltime_data, flow_data], axis=3)
 
+    X[year] = get_design_tensor(Z=df_year[['tt_avg'] + features_Z], n_links=n_links, n_days=n_days, n_hours=n_hours)
 
-# Prepare the training and validation dataset.
+    # For further internal processing, free flow travel times are stacked as the first feature of matrix X
+    tt_ff = get_design_tensor(Z=df_year[['tt_ff']], n_links=n_links, n_days=n_days, n_hours=n_hours)
+    X[year] = tf.concat([tt_ff, X[year]], axis=3)
+
+# plt.hist(df_year['tt_avg'])
+#
+# bins = np.linspace(-10, 10, 100)
+
+# plt.hist(df_year['tt_ff'], alpha=0.5, label='free flow travel times')
+# plt.hist(df_year['tt_avg'], alpha=0.5, label='average travel times')
+# plt.legend(loc='upper right')
+# plt.show()
+#
+# plt.scatter(df_year['tt_ff'],df_year['tt_avg'])
+# plt.legend(loc='upper right')
+# plt.show()
+
+## Training
+
+# Prepare the training and validation dataset
 # X, Y = tf.concat(list(X.values()),axis = 0), tf.concat(list(Y.values()),axis = 0)
 # X_train, X_val, Y_train, Y_val = train_test_split(X.numpy(), Y.numpy(), test_size=0.5, random_state=42)
 X_train, X_val, Y_train, Y_val = X[2019], X[2020], Y[2019], Y[2020]
 
 X_train, X_val, Y_train, Y_val = [tf.constant(i) for i in [X_train, X_val, Y_train, Y_val]]
 
-train_losses_dfs = {}
-val_losses_dfs = {}
-
 _EPOCHS = 10
 _BATCH_SIZE = 4
-_LR = 1e-1 #Default is 1e-3. With 1e-1, training becomes unstable
+_LR = 5e-1  # Default is 1e-3. With 1e-1, training becomes unstable
 
 # models = dict(zip(['m1', 'm2', 'm3', 'm4'], True))
 models = dict.fromkeys(['m1', 'm2', 'm3', 'm4'], False)
@@ -119,15 +188,44 @@ models = dict.fromkeys(['m1', 'm2', 'm3', 'm4'], False)
 # models['m3'] = True
 models['m4'] = True
 
-if models['m1']:
+# optimizer = tf.keras.optimizers.Adagrad(learning_rate=_LR)
+optimizer = tf.keras.optimizers.Adam(learning_rate=_LR)
 
-    print('\nmodel 1\n')
+
+equilibrator = Equilibrator(
+    network=fresno_network,
+    # paths_generator=paths_generator,
+    utility_function=utility_function,
+    max_iters=100,
+    method='fw',
+    iters_fw=50,
+    accuracy=1e-4,
+)
+
+column_generator = ColumnGenerator(equilibrator=equilibrator,
+                                   utility_function=utility_function,
+                                   n_paths=0,
+                                   ods_coverage=0.1,
+                                   ods_sampling='sequential',
+                                   # ods_sampling='demand',
+                                   )
+
+train_losses_dfs = {}
+val_losses_dfs = {}
+
+if models['m1']:
+    print('\nmodel 1: Benchmark of aesuelogit and isuelogit')
+
+    # optimizer = NGD(learning_rate=_LR)
 
     # Model 1 (Utility only)
     model_1 = AESUELOGIT(
+        key='model_1',
         network=fresno_network,
         dtype=tf.float64,
-        trainables={'theta': True, 'theta_links': False, 'q': False, 'alpha': False, 'beta': False, },
+        trainables={'theta': True, 'theta_links': False, 'psc_factor': False, 'q': False, 'alpha': False, 'beta': False},
+        equilibrator = equilibrator,
+        column_generator = column_generator,
         utility_function=utility_function,
         inits={
             'q': fresno_network.q.flatten(),
@@ -141,28 +239,31 @@ if models['m1']:
 
     train_losses_dfs['model_1'], val_losses_dfs['model_1'] = model_1.train(
         X_train, Y_train, X_val, Y_val,
-        optimizer = tf.keras.optimizers.Adam(learning_rate=_LR),
-        batch_size = _BATCH_SIZE,
-        lambdas = {'od': 0, 'theta': 0, 'tt': 1, 'flow': 1, 'bpr': 1},
-        epochs = _EPOCHS)
+        optimizer=optimizer,
+        batch_size=_BATCH_SIZE,
+        lambdas={'od': 0, 'theta': 0, 'tt': 0, 'flow': 1, 'bpr': 0},
+        epochs=_EPOCHS)
 
-    plot_predictive_performance(train_losses_df=train_losses_dfs['model_1'], val_losses_df=val_losses_dfs['model_1'])
+    plot_predictive_performance(train_losses=train_losses_dfs['model_1'], val_losses=val_losses_dfs['model_1'])
 
     print(f"theta = {dict(zip(utility_function.true_values.keys(), list(model_1.theta.numpy())))}")
     print(f"alpha = {model_1.alpha}, beta  = {model_1.beta}")
-    print("Avg abs diff between observed and estimated OD:",f"{np.mean(np.abs(model_1.q - fresno_network.q.flatten()))}")
+    print("Avg abs diff between observed and estimated OD:",
+          f"{np.mean(np.abs(model_1.q - fresno_network.q.flatten()))}")
 
 if models['m2']:
-    print('\nmodel 2\n')
+    print('\nmodel 2: OD + utility estimation with historic OD')
 
-    # Model 2 (Utility and ODE with historic OD)
     model_2 = AESUELOGIT(
+        key='model_2',
         network=fresno_network,
         dtype=tf.float64,
         trainables={'theta': True, 'theta_links': False, 'q': True, 'alpha': False, 'beta': False},
+        equilibrator=equilibrator,
+        column_generator=column_generator,
         utility_function=utility_function,
         inits={
-            'q': fresno_network.q.flatten(),
+            'q': fresno_network.q.flatten()[np.newaxis,:],
             'theta': np.array(list(utility_function.initial_values.values())),
             'beta': np.array([4]),
             'alpha': np.array([0.15])
@@ -171,28 +272,33 @@ if models['m2']:
 
     train_losses_dfs['model_2'], val_losses_dfs['model_2'] = model_2.train(
         X_train, Y_train, X_val, Y_val,
-        optimizer = tf.keras.optimizers.Adam(learning_rate=_LR),
-        batch_size = _BATCH_SIZE,
-        lambdas = {'od': 1, 'theta': 0, 'tt': 1, 'flow': 1, 'bpr': 0},
-        epochs = _EPOCHS)
+        optimizer=optimizer,
+        batch_size=_BATCH_SIZE,
+        lambdas={'od': 1, 'theta': 0, 'tt': 1, 'flow': 1, 'bpr': 0},
+        epochs=_EPOCHS)
 
-    plot_predictive_performance(train_losses_df=train_losses_dfs['model_2'], val_losses_df=val_losses_dfs['model_2'])
+    plot_predictive_performance(train_losses=train_losses_dfs['model_2'], val_losses=val_losses_dfs['model_2'])
 
     print(f"theta = {dict(zip(utility_function.true_values.keys(), list(model_2.theta.numpy())))}")
     print(f"alpha = {model_2.alpha}, beta  = {model_2.beta}")
-    print("Avg abs diff between observed and estimated OD:",f"{np.mean(np.abs(model_2.q - fresno_network.q.flatten()))}")
+    print("Avg abs diff between observed and estimated OD:",
+          f"{np.mean(np.abs(model_2.q - fresno_network.q.flatten()))}")
 
 if models['m3']:
-    print('\nmodel 3\n')
+
+    print('\nmodel 3: ODLUE + link performance parameters without historic OD matrix')
 
     # Model 3 (Utility, ODE, link performance parameters, no historic OD)
     model_3 = AESUELOGIT(
+        key='model_3',
         network=fresno_network,
         dtype=tf.float64,
-        trainables={'theta': True, 'theta_links': False, 'q': True, 'alpha': True, 'beta': True},
+        trainables={'theta': True, 'theta_links': False, 'psc_factor': True, 'q': True, 'alpha': True, 'beta': True},
+        equilibrator=equilibrator,
+        column_generator=column_generator,
         utility_function=utility_function,
         inits={
-            'q': fresno_network.q.flatten(),
+            'q': fresno_network.q.flatten()[np.newaxis,:],
             'theta': np.array(list(utility_function.initial_values.values())),
             'beta': np.array([4]),
             'alpha': np.array([0.15])
@@ -201,47 +307,52 @@ if models['m3']:
 
     train_losses_dfs['model_3'], val_losses_dfs['model_3'] = model_3.train(
         X_train, Y_train, X_val, Y_val,
-        optimizer = tf.keras.optimizers.Adam(learning_rate=_LR),
-        batch_size = _BATCH_SIZE,
-        lambdas = {'od': 0, 'theta': 0, 'tt': 0, 'flow': 1, 'bpr': 0},
-        epochs = _EPOCHS)
+        optimizer=optimizer,
+        batch_size=_BATCH_SIZE,
+        lambdas={'od': 0, 'theta': 0, 'tt': 1, 'flow': 1, 'bpr': 0},
+        epochs=_EPOCHS)
 
-    plot_predictive_performance(train_losses_df=train_losses_dfs['model_3'], val_losses_df=val_losses_dfs['model_3'])
+    plot_predictive_performance(train_losses=train_losses_dfs['model_3'], val_losses=val_losses_dfs['model_3'])
 
     print(f"theta = {dict(zip(utility_function.true_values.keys(), list(model_3.theta.numpy())))}")
     print(f"alpha = {model_3.alpha}, beta  = {model_3.beta}")
-    print("Avg abs diff between observed and estimated OD:",f"{np.mean(np.abs(model_3.q - fresno_network.q.flatten()))}")
-
+    print("Avg abs diff between observed and estimated OD:",
+          f"{np.mean(np.abs(model_3.q - fresno_network.q.flatten()))}")
 
 if models['m4']:
-    print('\nmodel 4\n')
+    print('\nmodel 4: Time specific utility and OD, link performance parameters, no historic OD')
 
     # Model 4 (Time specific Utility and ODE, link performance parameters, no historic OD)
     model_4 = AESUELOGIT(
+        key='model_4',
         network=fresno_network,
         dtype=tf.float64,
-        trainables={'theta': True, 'theta_links': False, 'q': True, 'alpha': True, 'beta': True},
+        trainables={'theta': True, 'theta_links': False, 'psc_factor': True, 'q': True, 'alpha': True, 'beta': True},
+        equilibrator=equilibrator,
+        column_generator=column_generator,
         utility_function=utility_function,
         inits={
-            'q': np.repeat(fresno_network.q.flatten()[np.newaxis,:],n_hours,axis =0),
-            'theta': np.repeat(np.array(list(utility_function.initial_values.values()))[np.newaxis,:],n_hours,axis =0),
+            'q': np.repeat(fresno_network.q.flatten()[np.newaxis, :], n_hours, axis=0),
+            'theta': np.repeat(np.array(list(utility_function.initial_values.values()))[np.newaxis, :], n_hours,
+                               axis=0),
             'beta': np.array([4]),
             'alpha': np.array([0.15])
         },
     )
     train_losses_dfs['model_4'], val_losses_dfs['model_4'] = model_4.train(
         X_train, Y_train, X_val, Y_val,
-        optimizer = tf.keras.optimizers.Adam(learning_rate=_LR),
-        batch_size = _BATCH_SIZE,
-        lambdas = {'od': 0, 'theta': 0, 'tt': 1, 'flow': 1, 'bpr': 0},
-        epochs = _EPOCHS)
+        optimizer=optimizer,
+        batch_size=_BATCH_SIZE,
+        lambdas={'od': 0, 'theta': 0, 'tt': 1, 'flow': 1, 'bpr': 0},
+        epochs=_EPOCHS)
 
-    plot_predictive_performance(train_losses_df=train_losses_dfs['model_4'], val_losses_df=val_losses_dfs['model_4'])
+    plot_predictive_performance(train_losses=train_losses_dfs['model_4'], val_losses=val_losses_dfs['model_4'])
 
     print(f"features = {utility_function.features}")
     print(f"theta = {model_4.theta.numpy()}")
     print(f"alpha = {model_4.alpha}, beta  = {model_4.beta}")
-    print("Avg abs diff between observed and estimated OD:",f"{np.mean(np.abs(model_4.q - fresno_network.q.flatten()))}")
+    print("Avg abs diff between observed and estimated OD:",
+          f"{np.mean(np.abs(model_4.q - fresno_network.q.flatten()))}")
 
 # model.predict_flow(X_val)
 
