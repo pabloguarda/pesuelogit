64953d033008e2dc712f4ce8081db9f90fbb1720
pabloguarda <pabloguarda@cmu.edu>
2022-09-16 10:29:55 -0400

    Update descriptive_statistics.py

--- src/aesuelogit/descriptive_statistics.py
+++ src/aesuelogit/descriptive_statistics.py
@@ -1,12 +1,22 @@
 import tensorflow as tf
 
 def error(actual: tf.constant, predicted: tf.constant):
+    # return tf.boolean_mask(predicted - actual, tf.math.is_finite(predicted - actual))
     return tf.boolean_mask(predicted - actual, tf.math.is_finite(predicted - actual))
+
+
+def l1norm(actual: tf.constant, predicted: tf.constant, weight = 1) -> tf.float64:
+    if weight == 0:
+        return tf.constant(0, tf.float64)
+    return tf.cast(weight*tf.norm(error(actual, predicted), 1),tf.float64)
+
 
 def mse(actual: tf.constant, predicted: tf.constant, weight = 1) -> tf.float64:
     if weight == 0:
         return tf.constant(0, tf.float64)
     return tf.cast(weight*tf.reduce_mean(tf.math.pow(error(actual, predicted), 2)),tf.float64)
+
+# (actual-predicted).numpy().flatten().shape
 
 def rmse(actual: tf.constant, predicted: tf.constant, weight = 1) -> tf.float64:
     if weight == 0:
@@ -17,6 +27,13 @@ def nrmse(actual: tf.constant, predicted: tf.constant, weight = 1) -> tf.float64
     if weight == 0:
         return tf.constant(0, tf.float64)
     return weight*rmse(actual, predicted)/tf.experimental.numpy.nanmean(actual)
+
+def mnrmse(actual: tf.constant, predicted: tf.constant, weight = 1) -> tf.float64:
+    """ Normalized rmse by the maximum observed value"""
+    if weight == 0:
+        return tf.constant(0, tf.float64)
+
+    return weight*rmse(actual, predicted)/tf.experimental.numpy.max(actual[~tf.experimental.numpy.isnan(actual)])
 
 def btcg_mse(actual: tf.constant, predicted: tf.constant, weight = 1):
     ''' Normalization used by Wu et al. (2018), TRC. This metric has more numerical issues than using MSE'''
